feca7c0277d9feeb942acda915cb3d37285a2d55
bruceliu <bruceliu1107@gmail.com>
2023-05-24 19:44:18 -0400

    Go to Graph button

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Import the Graph component
+import Graph from "./Graph";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Graph" component={Graph} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// HomeScreen component
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.homeContainer}>
+      <Text style={styles.usernameContainer}>Hi Bruce</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Graph")}
+      >
+        <Text style={styles.buttonText}>Go to Graph</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  homeContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  usernameContainer: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textShadowColor: "rgba(0, 0, 0, 0.4)",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 2,
+    marginTop: "3%",
+  },
+  button: {
+    backgroundColor: '#F5F5F5',
+    borderRadius: 10,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    marginTop: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
   },
 });
---
+++ Graph.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Graph = () => {
+  return (
+    <View>
+      <Text>I am graph 2</Text>
+    </View>
+  );
+};
+
+export default Graph;
